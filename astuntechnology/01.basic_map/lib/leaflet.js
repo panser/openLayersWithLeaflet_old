16f63971f8b62a8266e16ecea5fba8c1ce3b0308
Sergey Panov <gostroy@gmail.com>
2016-01-22 11:01:56 +0200

    01. add map with OSM tiles

---
+++ astuntechnology/01.basic_map/lib/leaflet.js
@@ -0,0 +1,13 @@
+
+var map = new L.Map('map', {
+    layers: [
+        new L.TileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+            }
+        )
+    ]
+});
+
+map.setView([52.5, -1.8], 6);
